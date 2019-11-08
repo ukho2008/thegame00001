7cc5ffb2a713c40f89fdd32905a67fda43c063bf
Eugen Zimmermann <ukho2008@rambler.ru>
2019-11-08 02:31:12 +0200

    Work in progress

--- main_game.py
+++ main_game.py
@@ -6,32 +6,61 @@ import player
 import blocks
 import pyganim
 
-WIN_WIDTH = 1280
-WIN_HEIGHT = 832
+WIN_WIDTH = 1024
+WIN_HEIGHT = 768
 DISPLAY = (WIN_WIDTH, WIN_HEIGHT)
 BACKGROUND = "#004400"
 PF_WIDTH = PF_HEIGHT = 64
 
+class Camera(object):
+    
+    def __init__(self, camera_func, width, height):
+        self.camera_func = camera_func
+        self.state = pygame.Rect(0, 0, width, height)
+
+    def apply(self, target):
+        return target.rect.move(self.state.topleft)
+
+    def update(self, target):
+        self.state = self.camera_func(self.state, target.rect)
+
+def camera_configure(camera, target_rect):
+    l, t, _, _ = target_rect
+    _, _, w, h = camera
+    l, t = -l+WIN_WIDTH / 2, -t+WIN_HEIGHT / 2
+
+    # Ограничения по границам
+    l = min(0, l)                            # Не движемся дальше левой
+    l = max(-(camera.width-WIN_WIDTH), l)    # Не движемся дальше правой
+    t = max(-(camera.height-WIN_HEIGHT), t)  # Не движемся дальше нижней
+    t = min(0, t)                            # Не движемся дальше верхней
+
+    return pygame.Rect(l, t, w, h)
+
 level = [
-    "********************",
-    "*                  *",
-    "*      ***         *",
-    "*                  *",
-    "*                  *",
-    "*           *****  *",
-    "*                  *",
-    "*                  *",
-    "*  **********      *",
-    "*                  *",
-    "*         ***      *",
-    "*                  *",
-    "********************"
+    "************************************",
+    "*                                  *",
+    "*      ***                         *",
+    "*                         **********",
+    "*                                  *",
+    "*           *****                  *",
+    "*                        ******    *",
+    "*******                            *",
+    "*                                  *",
+    "*               **********         *",
+    "*                                  *",
+    "*                              *****",
+    "*  **********                      *",
+    "*                   *******        *",
+    "*         ***                      *",
+    "*                                  *",
+    "************************************"
 ]
 platforms = []
 def main():
     pygame.init()
 
-    hero = player.Player(100, 100)
+    hero = player.Player(100, 600)
     left = right = up = False
 
     entities = pygame.sprite.Group()
@@ -43,6 +72,11 @@ def main():
     pygame.display.set_caption("THE GAME")
     bg = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
     bg.fill(pygame.Color(BACKGROUND))
+
+    total_level_width = len(level[0]) * PF_WIDTH
+    total_level_height = len(level) * PF_HEIGHT
+
+    camera = Camera(camera_configure, total_level_width, total_level_height)
 
     x = y = 0
     for row in level:
@@ -80,7 +114,10 @@ def main():
         
         bg.fill(pygame.Color(BACKGROUND))
         hero.update(left, right, up, platforms)
-        entities.draw(bg)
+        camera.update(hero)
+        for e in entities:
+            bg.blit(e.image, camera.apply(e))
+        #entities.draw(bg)
         main_win.blit(bg, (0, 0))
         pygame.display.flip()
 
